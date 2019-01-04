364cae600c6005e37643c002ca5988c5f2f7b0e0
Delbrel <antoine.delbrel@ynov.com>
2019-01-04 16:21:58 +0100

    initialisation

--- pong.py
+++ pong.py
@@ -1,11 +1,18 @@
 from tkinter import*
+from math import*
+
 
 fenetre = Tk()
 fenetre.title('pong')
 
+dx = 7
+dy = 7
+
 canvas = Canvas(fenetre, width = 725, height = 500, bg = 'black')
 
 button_quitter = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
+
+
 
 #creation de la balle
 ball = canvas.create_oval(342.5,230,382.5,270, fill = 'red')
@@ -14,8 +21,52 @@ ball = canvas.create_oval(342.5,230,382.5,270, fill = 'red')
 raquette_droite = canvas.create_rectangle(725,200,700,300, fill = 'white')
 raquette_gauche = canvas.create_rectangle(25,200,0,300, fill = 'white')
 
+#mouvement de la balle
+def jouer():
+    global fenetre
+
+    def move_ball():
+        global dx, dy
+        if canvas.coords(ball)[3] > 500 or canvas.coords(ball)[1] < 0:
+            dy *= -1
+        if len(canvas.find_overlapping(canvas.coords(raquette_droite)[0], canvas.coords(raquette_droite)[1], canvas.coords(raquette_droite)[2], canvas.coords(raquette_droite)[3])) > 1:
+            dx *= -1
+        if len(canvas.find_overlapping(canvas.coords(raquette_gauche)[0], canvas.coords(raquette_gauche)[1], canvas.coords(raquette_gauche)[2], canvas.coords(raquette_gauche)[3])) > 1:
+            dx *= -1
+            
+            
+        canvas.move(ball, dx, dy)
+        fenetre.after(20, move_ball)           
+        
+            
+    move_ball()   
+
+    
+#on fait bouger les raquettes
+def droite_haut(event):
+    canvas.move(raquette_droite, 0, -10)
+
+def droite_bas(event):
+    canvas.move(raquette_droite, 0, 10)
+   
+def gauche_haut(event):
+    canvas.move(raquette_gauche, 0, -10)
+
+def gauche_bas(event):
+    canvas.move(raquette_gauche, 0, 10)
+
+canvas.bind_all('<Up>', droite_haut)
+canvas.bind_all('<Down>', droite_bas)
+canvas.bind_all('<a>', gauche_haut)
+canvas.bind_all('<q>', gauche_bas)
+
+
 button_quitter.pack()
+
 canvas.pack()
+
+button_jouer = Button(fenetre, text = 'Jouer', command = jouer)
+button_jouer.pack()
 
 
 fenetre.mainloop()
